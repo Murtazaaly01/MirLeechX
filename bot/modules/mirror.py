43ec6da4444be5f08a2aaeba9e7138179857832c
Sourcery AI <>
2022-07-22 17:30:30 +0000

    'Refactored by Sourcery'

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -82,7 +82,7 @@ class MirrorListener(listeners.MirrorListeners):
                 with download_dict_lock:
                     download_dict[self.uid] = ZipStatus(name, m_path, size)
                 pswd = self.pswd
-                path = m_path + ".zip"
+                path = f"{m_path}.zip"
                 LOGGER.info(f'Zip: orig_path: {m_path}, zip_path: {path}')
                 if pswd is not None:
                     subprocess.run(["7z", "a", "-mx=0", f"-p{pswd}", path, m_path])
@@ -362,20 +362,13 @@ def _mirror(bot, update, isZip=False, extract=False, isQbit=False, isLeech=False
         link = link.split("://", maxsplit=1)
         link = f'{link[0]}://{ussr}:{pssw}@{link[1]}'
     pswd = mesg[0].split('pswd: ')
-    if len(pswd) > 1:
-        pswd = pswd[1]
-    else:
-        pswd = None
+    pswd = pswd[1] if len(pswd) > 1 else None
     link = re.split(r"pswd:|\|", link)[0]
     link = link.strip()
     reply_to = update.message.reply_to_message
     if reply_to is not None:
-        file = None
         media_array = [reply_to.document, reply_to.video, reply_to.audio]
-        for i in media_array:
-            if i is not None:
-                file = i
-                break
+        file = next((i for i in media_array if i is not None), None)
         if (
             not bot_utils.is_url(link)
             and not bot_utils.is_magnet(link)
